a4be2ef17f0233db5626a3707758c3ca16fcf939
Nicolas Guilherme <nicolas.gmop@gmail.com>
2020-11-02 17:36:46 -0300

    relembrando conceitos de node

--- src/index.js
+++ src/index.js
@@ -1,36 +1,46 @@
 const express = require('express');
+const { uuid } = require('uuidv4');
 
 const app = express();
 
 app.use(express.json());
 
-const projects = [];
+let projects = [];
 
 app.get('/projects', (request, response) => {
-  const { title, owner } = request.query;
-  console.log(title);
-  console.log(owner);
-  return response.json(projects);
+  const { title } = request.query;
+  const results = title
+    ? projects.filter(project => project.title.includes(title))
+    : projects;
+  return response.json(results);
 });
 
 app.post('/projects', (request, response) => {
   const { title, owner } = request.body;
-  console.log(title);
-  console.log(owner);
-  return response.json('criado com sucesso');
+  const project = { id: uuid(), title, owner };
+  projects.push(project);
+  return response.json(project);
 });
 
 app.put('/projects/:id', (request, response) => {
   const { id } = request.params;
-  console.log(id);
-  return response.json('Atualizado com sucesso');
+  const { title, owner } = request.body;
+  const projectIndex = projects.findIndex(project => project.id === id);
+  if (projectIndex < 0) { return response.status(400).json({ error: 'Projeto não encontrado.' }); }
+  const project = { id, title, owner };
+  projects[projectIndex] = project;
+  return response.json({ message: 'Projeto atualizado com sucesso!' });
 });
 
 app.delete('/projects/:id', (request, response) => {
-  return response.json([]);
+  const { id } = request.params;
+  const projectIndex = projects.findIndex(project => project.id === id);
+  if (projectIndex < 0) { return response.status(400).json({ error: 'Projeto não encontrado.' }); }
+  projects.splice(projectIndex, 1);
+  return response.json({ message: 'Projeto deletado com sucesso!' });
 });
 
-const PORT = 3333;
+const PORT = 3000;
 app.listen(PORT, 'localhost', () => {
-  console.log(`🚀 SERVIDOR INITIALIZED AT http://localhost:${PORT}`)
+  console.log(`🚀 SERVIDOR INITIALIZED AT http://localhost:${PORT}`);
 });
